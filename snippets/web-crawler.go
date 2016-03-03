44c758454421e32230f7d9356c274cc233057f5f
chansuke <moonset20@gmail.com>
2016-03-04 02:49:14 +0900

    Add select and web-crawler(not complete)

---
+++ snippets/web-crawler.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Fetcher interface {
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+func Crawl(url string, depth int, fetcher Fetcher) {
+	if depth <= 0 {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher)
+	}
+	return
+}
+
+func main() {
+	Crawl("http://golang.org/", 4, fetcher)
+}
